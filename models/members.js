e3ddf8f477cb3b9da1a1ffe5e39d1dcd2ddef4ac
Colin Whitney <colin.whitney@gmail.com>
2020-10-30 12:42:34 -0400

    created users and members models

---
+++ models/members.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const memberSchema = Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  picture: String,
+  occupation: String,
+  school: String,
+  age: Number,
+  location: String,
+  interests: [String],
+  aboutMe: String,
+  isGreen: { type: Boolean, required: true }
+});
+
+const Member = mongoose.model('Member', memberSchema);
+module.exports = Member;
