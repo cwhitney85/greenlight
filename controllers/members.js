27b0d4a67707db37270783371d05d395abd4c84e
Colin Whitney <colin.whitney@gmail.com>
2020-10-30 17:48:52 -0400

    creating profile page; confirmed form for new members working

--- controllers/members.js
+++ controllers/members.js
@@ -2,11 +2,28 @@ const express = require('express');
 const members = express.Router();
 const Member = require('../models/members.js');
 
+members.get('/profile', (req, res) => {
+  res.render('./members/profile.ejs')
+})
+
 members.get('/new', (req, res) => {
   res.render('./members/new.ejs')
 });
 
-
+members.post('/', (req, res) => {
+  req.body.interests = req.body.interests.split(',');
+  if (req.body.isGreen === 'green') {
+    req.body.isGreen = true;
+  } else {
+    req.body.isGreen = false;
+  }
+  Member.create(req.body, (err, member) => {
+    console.log('member is created', member)
+    res.redirect('/members/profile', {
+      member: member,
+    })
+  })
+});
 
 
 module.exports = members;
