5aee711d8fb4d1bc083aa6a642beb4355c5d6de6
Colin Whitney <colin.whitney@gmail.com>
2020-10-30 12:08:03 -0400

    added express session to server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
+const session = require('express-session');
 const app = express ();
 const db = mongoose.connection;
 //___________________
@@ -43,6 +44,14 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+// use express sessions
+app.use(
+  session({
+    secret: process.env.SECRET || 'MoreCowbell',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 //___________________
 // Routes
