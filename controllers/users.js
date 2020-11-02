94db34739e9449cf85634d86887fdc1fc09835b3
Colin Whitney <colin.whitney@gmail.com>
2020-11-02 01:09:00 -0500

    fixed new users route

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const users = express.Router();
 const User = require('../models/users.js');
 
 
-users.get('/users/new', (req, res) => {
+users.get('/new', (req, res) => {
   res.render('./users/new.ejs');
 })
 
