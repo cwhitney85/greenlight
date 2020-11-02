90b9e4382e53410061f0774a2e8f7d4e4d48d7a9
Colin Whitney <colin.whitney@gmail.com>
2020-11-02 16:32:02 -0500

    template for sessions/new.ejs rendering

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -4,7 +4,7 @@ const sessions = express.Router();
 const User = require('../models/users.js');
 
 sessions.get('/new', (req, res) => {
-  res.send('this is your log in page. New sessions start here')
+  res.render('./sessions/new.ejs')
 })
 
 
