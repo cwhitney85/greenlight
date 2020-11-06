a4dfabbb9a50c8cf1db427fff4947b02bc7efec3
Colin Whitney <colin.whitney@gmail.com>
2020-11-06 07:59:19 -0500

    update route for member working

--- controllers/members.js
+++ controllers/members.js
@@ -2,6 +2,13 @@ const express = require('express');
 const members = express.Router();
 const Member = require('../models/members.js');
 const sessions = require('./sessions.js');
+const isAuthenticated = (req, res, next) => {
+  if (req.session.currentUser) {
+    return next()
+  } else {
+    res.redirect('/sessions/new')
+  }
+};
 
 members.get('/:username/profile', (req, res) => {
   Member.findOne({ username: req.params.username }, (err, member) => {
@@ -15,6 +22,15 @@ members.get('/:username/profile', (req, res) => {
 members.get('/new', (req, res) => {
   res.render('./members/new.ejs', {
     currentUser: req.session.currentUser
+  })
+});
+
+members.get('/:username/edit', (req, res) => {
+  Member.findOne( { username: req.params.username }, (err, member) => {
+    res.render('./members/edit.ejs', {
+      member: member,
+      currentUser: req.session.currentUser
+    })
   })
 });
 
@@ -32,6 +48,17 @@ members.post('/', (req, res) => {
   })
 });
 
+members.put('/:username/profile', isAuthenticated, (req, res) => {
+  req.body.interests = req.body.interests.split(',');
+  if (req.body.isGreen === 'green') {
+    req.body.isGreen = true;
+  } else {
+    req.body.isGreen = false;
+  }
+  Member.findOneAndUpdate({ username: req.params.username }, req.body, { new: true }, (err, member) => {
+    res.redirect('/members/' + member.username + '/profile')
+  })
+});
 
 
 
