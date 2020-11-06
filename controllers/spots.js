e45fb86d008f0860a58d5c1ced99eab255ea8cd5
Colin Whitney <colin.whitney@gmail.com>
2020-11-06 09:25:04 -0500

    post route working for spots

--- controllers/spots.js
+++ controllers/spots.js
@@ -2,7 +2,13 @@ const bcrypt = require('bcrypt');
 const express = require('express');
 const spots = express.Router();
 const Spot = require('../models/spots.js');
-
+const isAuthenticated = (req, res, next) => {
+  if (req.session.currentUser) {
+    return next()
+  } else {
+    res.redirect('/sessions/new')
+  }
+};
 
 spots.get('/new', (req, res) => {
   res.render('./spots/new.ejs', {
@@ -19,14 +25,7 @@ spots.get('/:name/edit', (req, res) => {
   })
 });
 
-spots.get('/:name', (req, res) => {
-  Spot.findOne({ name: req.params.name }, (err, spot) => {
-    res.render('./spots/profile.ejs', {
-      spot: spot,
-      currentUser: req.session.currentUser,
-    })
-  })
-});
+
 
 spots.post('/', (req, res) => {
   req.body.deals = req.body.deals.split(',');
@@ -36,9 +35,32 @@ spots.post('/', (req, res) => {
     req.body.is21Plus = false;
   }
   Spot.create(req.body, (err, spot) => {
-    res.redirect('/spots/' + req.body.name)
+    console.log(spot)
+    res.redirect('/spots/' + spot.name)
   })
 });
 
+
+spots.put('/:name', isAuthenticated, (req, res) => {
+  req.body.deals = req.body.deals.split(',');
+  if (req.body.is21Plus === "yes") {
+    req.body.is21Plus = true;
+  } else {
+    req.body.is21Plus = false;
+  }
+  Spot.findOneAndUpdate({ name: req.params.name }, req.body, { new: true }, (err, updatedSpot) => {
+    console.log(updatedSpot)
+    res.redirect('/spots/' + updatedSpot.name)
+  })
+});
+
+spots.get('/:name', (req, res) => {
+  Spot.findOne({ name: req.params.name }, (err, spot) => {
+    res.render('./spots/profile.ejs', {
+      spot: spot,
+      currentUser: req.session.currentUser,
+    })
+  })
+});
 
 module.exports = spots;
